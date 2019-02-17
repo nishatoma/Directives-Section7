0c1f297c0516854fbcf6cc980df80650a2708daf
Nisha Shlaymoon <nishatoma@gmail.com>
2019-02-17 15:35:33 -0500

    Add HostListener to listen to mouse events

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,9 +1,9 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
-export class BetterHighlightDirective implements OnInit{
+export class BetterHighlightDirective implements OnInit {
 
   // This time instead of accessing the nativeElement using dom we will
   // use the renderer instead.
@@ -17,8 +17,24 @@ export class BetterHighlightDirective implements OnInit{
     // Why is this the better approach? Angular is not just in the browser,
     // it also works with service workers, where we might not have access to the dom
     // if you try to change the dom in these circumstances, you will get an error.
+
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'blue');
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'white');
+  }
+
+  // This will trigger, whenever some event occurs. The event
+  // is specified as an argument as a string in the HostListener
+  // method. 'mouseenter' is one of the supported events
+  @HostListener('mouseenter') mouseOver(eventData: Event) {
     this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'blue');
     this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'white');
+  }
+
+  // Another listener we can add here is for when the mouse is no longer hovering.
+  // 'mouseleave' is another event supported by Angular.
+  @HostListener('mouseleave') mouseLeave(eventData: Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', 'white');
+    this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'black');
   }
 
 }
